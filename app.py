414f19bc49ebf384d9deb56c08973b2408041cf0
rahuljha21 <77482606+rahuljha21@users.noreply.github.com>
2021-05-07 15:17:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,13 @@ def user():
   N=st.sidebar.slider('Nitrogen',1,140)
   P=st.sidebar.slider('Phosphorus',0,145)
   K=st.sidebar.slider('Potassium',1,200)
-  N=st.sidebar.slider('Nitrogen',0,140)
   temperature=st.sidebar.slider('Temperature(celsuis)',9,45)
   humidity=st.sidebar.slider('Humidity',10,100)
   ph=st.sidebar.slider('Nitrogen',3.5,10.0)
   rainfall=st.sidebar.slider('Rainfall',20,300)
+  data={'N':N,'P':P,'K':K,'temperature':temperature,'humidity':humidity,'ph':ph,'rainfall':rainfall}
+  data=pd.DataFrame(data,index=[0])
+  return data
 
 data=user()
 st.subheader('User Input Parameter')
@@ -39,9 +41,8 @@ y=encode.transform(y)
 models=RandomForestClassifier()
 models.fit(x,y)
 
-
 test=models.predict(data)
 
 test=encode.inverse_transform(test)
-st.suheader('The crop you should grow ')
+st.subheader('The crop you should grow ')
 st.write(test)
